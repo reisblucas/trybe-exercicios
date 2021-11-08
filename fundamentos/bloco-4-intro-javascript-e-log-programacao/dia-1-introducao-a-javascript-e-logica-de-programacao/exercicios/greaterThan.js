ca65877a7a87a35afe68878871b3a2416ca202fb
Lucas B. Reis <lucasb.reis@hotmail.com>
2021-11-08 14:55:43 -0300

    Sobe exercicios ate o 6

---
+++ fundamentos/bloco-4-intro-javascript-e-log-programacao/dia-1-introducao-a-javascript-e-logica-de-programacao/exercicios/greaterThan.js
@@ -0,0 +1,4 @@
+const a = 10;
+const b = 7;
+
+console.log(a < b);
---
+++ fundamentos/bloco-4-intro-javascript-e-log-programacao/dia-1-introducao-a-javascript-e-logica-de-programacao/exercicios/mathOperators.js
@@ -0,0 +1,9 @@
+// Fazer um programa com cada operador
+const a = 12;
+const b = 5;
+
+console.log('Adição: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Resto da operação: ' + (a % b));
