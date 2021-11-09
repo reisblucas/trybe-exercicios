78bc97aa653d6b3e7c9889e10364fea96c0d271f
Lucas B. Reis <lucasb.reis@hotmail.com>
2021-11-09 16:46:15 -0300

    Conclui o Bonus

--- fundamentos/bloco-4-intro-javascript-e-log-programacao/dia-2-arrays-loop-for/exercicios/bonus-Array.js
+++ fundamentos/bloco-4-intro-javascript-e-log-programacao/dia-2-arrays-loop-for/exercicios/bonus-Array.js
@@ -1,5 +1,5 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ */
 /* // Bônus 1
 
 console.log(numbers.sort(function(a, b){return a-b}));
@@ -12,7 +12,7 @@ console.log(numbers.sort(function(a, b){return b-a}));
 
 // Bônus 3
 
-let newArray = [];
+/* let newArray = [];
 
 for (let i = 0; i < (numbers.length); i += 1) {
     const multipleNext = numbers[i] * numbers[i+1];
@@ -24,4 +24,22 @@ for (let i = 0; i < (numbers.length); i += 1) {
     
 
 }
-console.log(newArray);
+console.log(newArray); */
+
+// Bônus 3-1 - Concluído
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let resultado = [];
+
+for (let i = 0; i < numbers.length; i+=1) {
+        if (i < (numbers.length - 1)) {
+            multiplyNext = numbers[i] * numbers[i+1];
+            resultado.push(multiplyNext);
+        } else {
+            multiplyPer2 = numbers[i] * 2;
+            resultado.push(multiplyPer2);
+        }
+}
+
+console.log('Nova array: ', resultado);
