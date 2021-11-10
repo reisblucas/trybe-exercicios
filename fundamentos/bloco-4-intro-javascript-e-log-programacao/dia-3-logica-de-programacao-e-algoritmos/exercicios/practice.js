bd5ae8fd4d333f861c61b93c5ac70c536bbeca33
Lucas B. Reis <lucasb.reis@hotmail.com>
2021-11-09 22:26:38 -0300

    Sobe exercicio 1 e 2

---
+++ fundamentos/bloco-4-intro-javascript-e-log-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios/practice.js
@@ -0,0 +1,75 @@
+// Exercicio - 1
+
+// let n = 5;
+
+
+// if (n > 1) {
+//     for (let i = 0; i < n; i+=1) {
+//         let square = [];
+//         for (let j = 0; square.length < n; j+=1) {
+//             square.push('*')
+//         }
+//         if (square.length === n) {
+//             console.log(square)
+//         }
+//     }
+// }
+
+// Exercicio - 2
+
+// let n = 5;
+
+
+// if (n > 1) {
+//     for (let i = 0; i < n; i+=1) {
+//         let square = [];
+//         for (let j = 0; square.length < n; j+=1) {
+//             square.push('*');
+//         }
+//         console.log(square);
+//     }
+// }
+
+// //Exercicio - 1.2
+
+// let n = 0;
+
+// if (n > 1) {
+//     let asterisc = '*';
+//     let line = '';
+//     for (let i = 0; i < n; i+=1) {
+//         line = line + asterisc;
+//     }
+//     for (let i = 0; i < n; i += 1) {
+//         console.log(line);
+//     }
+// } else {
+//     console.log('"n" menor que 2, insira outro número.')
+// }
+
+// // Exercicio 2
+
+// const n = 5;
+
+// let asterisc = '*';
+// let line = '';
+// for (let i = 0; i < n; i+=1) {
+//     line = line + asterisc;
+//     console.log(line);
+// }
+
+// Exercicio 3
+
+const n = 5;
+
+let asterisc = '*';
+let line = '';
+
+for (let i = 0; i < n; i += 1) {
+    if () {
+
+    }
+    line += line;
+    
+    console.log(line);
+}
