2b7e762c0d7b040cbb7db5aec4aa4b4ebd1d8fa5
Lucas B. Reis <lucasb.reis@hotmail.com>
2021-11-16 22:59:11 -0300

    Sobe Exercicio 2 - Incompleto - Falta o 2.7

--- fundamentos/bloco-4-intro-javascript-e-log-programacao/dia-4-objetos-e-funcoes/exercicios/exercicio2.js
+++ fundamentos/bloco-4-intro-javascript-e-log-programacao/dia-4-objetos-e-funcoes/exercicios/exercicio2.js
@@ -51,7 +51,7 @@
 
 // // Exercicio 2.3
 
-// let array = [10, 222, -33, -40, -2, 50, 555, -1];
+// let array = [10, 222, -33, -40, -2, 50, -555, -1];
 
 // function lessIndex () {
 //     let less = array[0];
@@ -71,26 +71,69 @@
 
 // Exercicio 2.4
 
-let nameList = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// let nameList = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
-function greaterWord () {
-    let name1;
-    let name2;
-    let longestName;
-    for (let i = 0; i < nameList.length; i += 1) {
-        for (let j = 1; j < nameList.length; j += 1) {
-            name1 = nameList[i];
-            name2 = nameList[j];
-            if (name1.length < name2.length) {
-                continue
-            } else {
-                longestName = name1;
-            }
-        }
-    }
-    return longestName;
+// function greaterWord () {
+//     let resultado = nameList[0];
+//     for(let i = 0; i < nameList.length - 1; i += 1) {
+//         if (nameList[i] > nameList[i + 1]) {
+//             if (nameList[i] > resultado) {
+//                 resultado = nameList[i + 1];
+//             }
+//         }
+//     }
+//     return resultado;
+// }
+
+// Exercicio 2.5
+
+// let array = [2, 3, 2, 5, 8, 2, 3, 5, 5, 5, 6, 7];
+
+// function highestRepeat() {
+//     let object = {};
+//     for (let i = 0; i < array.length; i += 1) {
+//         if (object[array[i]]) {
+//             object[array[i]] += 1
+//         } else {
+//             object[array[i]] = 1
+//         }
+//     }
+//     let objectLength = Object.keys(object).length;
+//     let objectReversed = Object.keys(object).reverse()
+//     console.log(objectLength);
+//     console.log(objectReversed)
+
+//     console.log(objectReversed[0] == 8)
+//     // limite de 10 no contador
+//     let resultado = 0;
+//     for (let i = 0; i < 10; i += 1) {
+//         if (object[i] > object[i + 1]) {
+//             resultado = object[i];
+//         } 
+//     }
+    
+//     return resultado;
+// }
+// console.log(highestRepeat());
+
+// Exercicio 2.6
+
+// function sum(n) {
+//     if (n === 1) {
+//         return n;
+//     } else {
+//         let resultado = 0;
+//         for (let i = 1; i <= n; i += 1) {
+//             resultado += i;
+//         }
+//         return resultado;
+//     }
+// }
+
+// console.log(sum(256))
+
+// Exercicio 2.7
+
+function endVerify (a, b) {
+    
 }
-
-console.log(greaterWord());
-
-console.log(nameList.length[1]);
