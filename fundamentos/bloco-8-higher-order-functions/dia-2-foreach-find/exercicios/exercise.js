3c071032985819c1c47d7b26f7016ac6ca840271
Lucas B. Reis <lucasb.reis@hotmail.com>
2021-12-10 21:38:37 -0300

    Sobe exercicio 3 - 8.2

--- fundamentos/bloco-8-higher-order-functions/dia-2-foreach-find/exercicios/exercise.js
+++ fundamentos/bloco-8-higher-order-functions/dia-2-foreach-find/exercicios/exercise.js
@@ -64,3 +64,4 @@ const books = [
 // Adicione o código do exercício aqui:
 
 module.exports = books;
+
--- fundamentos/bloco-8-higher-order-functions/dia-2-foreach-find/exercicios/exercise3.js
+++ fundamentos/bloco-8-higher-order-functions/dia-2-foreach-find/exercicios/exercise3.js
@@ -1,1 +1,23 @@
 const books = require('./exercise');
+
+const getNameBook = arr => {
+  let nameBook;
+
+  let manipulate = 0;
+  let indexOfManipulate = 0;
+  arr.forEach( (element, index) => {
+    const length = element.name.length;
+    if (length === 26) {
+      manipulate = length;
+      indexOfManipulate = index;
+    }
+  });
+  // console.log(manipulate)
+  // console.log(indexOfManipulate)
+  
+  nameBook = Object.values(arr[indexOfManipulate].name.split()).join('')
+
+  return nameBook;
+};
+
+console.log(getNameBook(books));
