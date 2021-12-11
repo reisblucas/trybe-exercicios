db0ecdc0406db0d3ac70a4a0ad52dbef1f2ea51d
Lucas B. Reis <lucasb.reis@hotmail.com>
2021-12-10 22:31:26 -0300

    Sobe exercicio 4 - 8.2

--- fundamentos/bloco-8-higher-order-functions/dia-2-foreach-find/exercicios/exercise4.js
+++ fundamentos/bloco-8-higher-order-functions/dia-2-foreach-find/exercicios/exercise4.js
@@ -1,1 +1,13 @@
 const books = require('./exercise');
+
+books.sort((a, b) => {
+  if (a.releaseYear < b.releaseYear) {
+    return 1;
+  }
+  if (a.releaseYear > b.releaseYear) {
+    return -1;
+  }
+  return 0;
+});
+
+console.log(books);
