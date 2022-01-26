0bdc9b3444694fb2b34cdafce835a5545dd6a960
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-01-26 18:43:13 -0300

    Fixacao 11.1

--- front-end/bloco-11-components-state-events-forms/dia-1-components-state-events/fixation/fix-this-inside-function/src/App.js
+++ front-end/bloco-11-components-state-events-forms/dia-1-components-state-events/fixation/fix-this-inside-function/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      countClick: 0,
+    }
+  }
+
+  handleClick() {
+    this.setState((previousState, _props) => ({
+      countClick: previousState.countClick + 1,
+    }))
+  }
+
+  render() {
+    /* No React, precisamos dizer explicitamente que queremos uma função da nossa classe
+    através da sintaxe `this.minhaFuncao` para usá-la num evento */
+    return <button type="button" onClick={this.handleClick}>{this.state.countClick}</button>;
+  }
 }
 
 export default App;
+
+
+/*  retorno do this na Classe App
+  App {
+    context: {}
+    props: {}
+    refs: {}
+    state: null
+    updater: { isMounted: ƒ, enqueueSetState: ƒ, enqueueReplaceState: ƒ, enqueueForceUpdate: ƒ }
+    _reactInternalFiber: FiberNode { tag: 1, key: null, stateNode: App, elementType: ƒ, type: ƒ, …}
+    _reactInternalInstance: {_processChildContext: ƒ}
+    isMounted: (...)
+    replaceState: (...)
+    __proto__: Component
+      constructor: class App
+      isMounted: (...)
+      render: ƒ render()
+      replaceState: (...)
+      __proto__: {...}
+    // ... Continua
+    }
+*/
