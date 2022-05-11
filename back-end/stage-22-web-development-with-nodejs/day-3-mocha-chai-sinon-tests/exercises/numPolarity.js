c64ccc0caf0ae08f1f55bf727125587e477536af
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-05-11 19:57:14 -0300

    :sparkles: feat: exercise 2 and 3

--- back-end/stage-22-web-development-with-nodejs/day-3-mocha-chai-sinon-tests/exercises/numPolarity.js
+++ back-end/stage-22-web-development-with-nodejs/day-3-mocha-chai-sinon-tests/exercises/numPolarity.js
@@ -1,4 +1,8 @@
-const numPolarity = (x) => {
+const rs = require('readline-sync');
+
+const numPolarity = () => {
+  const x = rs.questionInt("Insert your number: ");
+
   if (typeof x !== 'number') { return "Please, insert a number."}
   if (x === 0) { return "It's a neutral number!"; }
   if (x < 0) { return "It's a negative number!"; }
--- back-end/stage-22-web-development-with-nodejs/day-3-mocha-chai-sinon-tests/exercises/tests/numPolarity.test.js
+++ back-end/stage-22-web-development-with-nodejs/day-3-mocha-chai-sinon-tests/exercises/tests/numPolarity.test.js
@@ -1,18 +1,31 @@
 const { expect } = require('chai');
 const numPolarity = require('../numPolarity');
+const sinon = require('sinon');
+const rs = require('readline-sync');
 
 describe("The function verifies the num polarity", () => {
+  afterEach(() => {
+    rs.questionInt.restore();
+  });
+
   it("Verifies when number is neutral", () => {
-    expect(numPolarity(0)).to.be.equal("It's a neutral number!");
+    sinon.stub(rs, "questionInt").returns(0);
+    expect(numPolarity()).to.be.equal("It's a neutral number!");
   })
 
   it("Verifies when number is positive", () => {
-    expect(numPolarity(2)).to.be.equal("It's a positive number!");
+    sinon.stub(rs, "questionInt").returns(2);
+    expect(numPolarity()).to.be.equal("It's a positive number!");
   })
 
   it("Verifies when number is negative", () => {
-    expect(numPolarity(-2)).to.be.equal("It's a negative number!");
+    sinon.stub(rs, "questionInt").returns(-10);
+    expect(numPolarity()).to.be.equal("It's a negative number!");
   })
 
   // make tests for not to be equal negative, positive and neutral...
+  it ("Verifies if the input it's different of number", () => {
+    sinon.stub(rs, "questionInt").returns("I'm not a number!");
+    expect(numPolarity()).to.be.equal('Please, insert a number.')
+  })
 });
