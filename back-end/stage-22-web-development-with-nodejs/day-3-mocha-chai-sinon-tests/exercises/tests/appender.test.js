0d007c2f16d98a7ddc793a2376cdaaafee361e27
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-05-11 21:31:27 -0300

    :sparkles: feat: appender

---
+++ back-end/stage-22-web-development-with-nodejs/day-3-mocha-chai-sinon-tests/exercises/tests/appender.test.js
@@ -0,0 +1,23 @@
+// const { expect } = require('chai');
+// const sinon = require('sinon');
+// const fs = require('fs');
+
+// const appender = require('../appender');
+
+// describe("Write data in some file", () => {
+//   beforeEach(() => {
+//     sinon.stub(fs, "appendFileSync").returns("Input: 0 | Output: It's a neutral number!");
+//   });
+
+//   afterEach(() => {
+//     sinon.appendFileSync.restore();
+//   });
+
+//   describe("The function need to receive 2 params", () => {
+
+//   });
+
+//   describe("After data has been writed, return 'ok'", () => {
+
+//   });
+// });
