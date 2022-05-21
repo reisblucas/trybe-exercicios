a441d53571a7765045f05998bc53ff1ce6dba63b
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-05-20 22:56:23 -0300

    :sparkles: feat: create pool connection with db

--- back-end/stage-23-software-architecture/day-2-sa-controller-and-service-layer/cep-lookup/src/db/index.js
+++ back-end/stage-23-software-architecture/day-2-sa-controller-and-service-layer/cep-lookup/src/db/index.js
@@ -1,2 +1,10 @@
-const mysql = require('mysql2/promises');
-const Joi = require('joi');
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+});
+
+module.exports = connection;
