e3ce8e16a4ff65af2f5cf8f04777488840c71fd0
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-05-23 18:44:04 -0300

    :bug: fix: getAllOcurrences

--- back-end/stage-23-software-architecture/day-3-rest-restful/sd-xp-a-live-lecture/ocorrencias-api/services/ocurrencesServices.js
+++ back-end/stage-23-software-architecture/day-3-rest-restful/sd-xp-a-live-lecture/ocorrencias-api/services/ocurrencesServices.js
@@ -1,11 +1,7 @@
 const { getAllOcurrences: getAllOcurrencesModel } = require('../models/ocurrencesModel');
 
 const getAllOcurrencesService = () => {
-  const response = getAllOcurrencesModel();
-
-  console.log(response);
-
-  return response;
+  return getAllOcurrencesModel();
 };
 
 module.exports = {
