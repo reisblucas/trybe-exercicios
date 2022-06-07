117250b1870d2b0c3bd6591de1b62a53244f3f64
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-06-06 22:43:02 -0300

    🔧 (config-seq): set environment process in config

--- back-end/stage-24-nodejs-orm-and-authentication/exercises/config/config.js
+++ back-end/stage-24-nodejs-orm-and-authentication/exercises/config/config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER || "root",
+    "password": proess.env.MYSQL_PASS || null,
+    "database": proess.env.MYSQL_DB || "database_development",
+    "host": proess.env.MYSQL_HOST || "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
