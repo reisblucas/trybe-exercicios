3b9e8102df5fcfacda47dc7b14611bfec4255677
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-05 13:43:16 -0300

    feat(exe-2): add enum for months of the year

---
+++ back-end/stage-26-typescript/day-1-intro-typescript/exercises/dist/year.js
@@ -0,0 +1,17 @@
+"use strict";
+var EYear;
+(function (EYear) {
+    EYear[EYear["January"] = 1] = "January";
+    EYear[EYear["February"] = 2] = "February";
+    EYear[EYear["March"] = 3] = "March";
+    EYear[EYear["April"] = 4] = "April";
+    EYear[EYear["May"] = 5] = "May";
+    EYear[EYear["June"] = 6] = "June";
+    EYear[EYear["July"] = 7] = "July";
+    EYear[EYear["August"] = 8] = "August";
+    EYear[EYear["September"] = 9] = "September";
+    EYear[EYear["October"] = 10] = "October";
+    EYear[EYear["November"] = 11] = "November";
+    EYear[EYear["December"] = 12] = "December";
+})(EYear || (EYear = {}));
+console.log(EYear);
---
+++ back-end/stage-26-typescript/day-1-intro-typescript/exercises/src/year.ts
@@ -0,0 +1,16 @@
+enum EYear {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+
+console.log(EYear);
