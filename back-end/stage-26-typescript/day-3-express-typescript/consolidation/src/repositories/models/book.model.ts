5dccbab05abbcfd3a23c842f72f0b744211a1cd8
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-06 17:11:52 -0300

    chore(models): book model added with class method

---
+++ back-end/stage-26-typescript/day-3-express-typescript/consolidation/src/repositories/models/book.model.ts
@@ -0,0 +1,27 @@
+import Book from "interfaces/book.interface";
+import { Pool, ResultSetHeader } from "mysql2/promise";
+
+export default class BookModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Book[]> {
+    const [rows] = await this.connection.execute(
+      'SELECT * FROM books;'
+    );
+    return rows as Book[];
+  }
+
+  public async create(book: Book): Promise<Book> {
+    const { title, price, author, isbn } = book;
+    const [dataInserted] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?);',
+      [title, price, author, isbn],
+    );
+    const { insertId } = dataInserted;
+    return { id: insertId, ...book };
+  }
+}
