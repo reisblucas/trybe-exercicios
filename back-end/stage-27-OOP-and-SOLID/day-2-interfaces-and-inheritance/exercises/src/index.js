1307b3ae3a3446ff00fc184667bbad2895281b54
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-12 20:45:06 -0300

    feat(exercises): daily exercises 27.2

---
+++ back-end/stage-27-OOP-and-SOLID/day-2-interfaces-and-inheritance/exercises/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Person_1 = require("./class/Person");
+var Biaa = new Person_1.default('Biaaa', '1');
+console.log(Biaa.name);
---
+++ back-end/stage-27-OOP-and-SOLID/day-2-interfaces-and-inheritance/exercises/src/index.ts
@@ -0,0 +1,4 @@
+import Person from "./class/Person";
+
+const Biaa = new Person('Biaaa', '1');
+console.log(Biaa.name);
